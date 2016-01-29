dabdc86821aa8d8572d13a7adb356e20a3d63bf4
Nicholas Castaneda <npcastaneda@gmail.com>
2016-01-28 21:21:40 -0800

    prof pic added

--- db/seed.js
+++ db/seed.js
@@ -18,6 +18,7 @@ exports.seedAdmins = function seedAdmins(){
           "email": "nick@nite.com",
           "phoneNumber": "999-999-9999",
           "password": "nick",
+          "profilePicture": "http://i.imgur.com/Gl32UOu.jpg",
           "admin": true
         }
       }),
@@ -83,17 +84,20 @@ exports.seedRestaurants = function seedRestaurants(){
           {
             "item": "Cheese Rolls",
             "price": "$1.00",
-            "available": true
+            "available": true,
+            "img_url": "http://www.portosbakery.com/sites/default/files/styles/product-detail250/public/Cheese%20Roll%20Dozen.jpg?itok=2TSPoohN"
           },
           {
             "item": "Guava Cheese",
             "price": "$2.50",
-            "available": true
+            "available": true,
+            "img_url": "http://s3.amazonaws.com/foodspotting-ec2/reviews/1118402/thumb_600.jpg?1323566474"
           },
           {
-            "item": "Regular cake (with stuff)",
+            "item": "Birthday Cake",
             "price": "$12.00",
-            "available": true
+            "available": true,
+            "img_url": "http://www.portosbakery.com/sites/default/files/styles/product-detail250/public/Parisian%20Chocolate%20Cake%209%20inches%20round.jpg?itok=MK5UUai2"
           }
         ]
       }),
@@ -106,16 +110,19 @@ exports.seedRestaurants = function seedRestaurants(){
           {
             "item": "Mango Sticky Rice",
             "price": "$3.00",
+            "img_url": "http://2.bp.blogspot.com/-m9h7UOSQjC4/Tc6nO64pA0I/AAAAAAAAFJc/lkCtZeN4ios/s1600/mango+sticky+rice.jpg",
             "available": true
           },
           {
             "item": "Fried Bananas",
             "price": "$2.00",
+            "img_url": "https://s-media-cache-ak0.pinimg.com/236x/68/1b/ff/681bff59297d7b076530dc42d0eaa52d.jpg",
             "available": true
           },
           {
             "item": "Tapioca Ballz",
             "price": "$1.12",
+            "img_url": "http://33.media.tumblr.com/941b2b0c87a689e2c3ee7f0903d739d8/tumblr_inline_n6d630Qlx71sg7kqe.jpg",
             "available": true
           }
         ]
@@ -129,21 +136,25 @@ exports.seedRestaurants = function seedRestaurants(){
           {
             "item": "McFlurry",
             "price": "$2.00",
+            "img_url": "http://vignette2.wikia.nocookie.net/ronaldmcdonald/images/6/6b/Screen_Shot_2011-12-05_at_9.08.27_PM.png/revision/latest?cb=20111206030909",
             "available": true,
           },
           {
             "item": "Apple Pie",
             "price": "$1.00",
+            "img_url": "http://sbrownehr.com/wp-content/uploads/2015/03/McDonalds-Apple-Pie.jpg",
             "available": false,
           },
           {
             "item": "Ice Cream",
             "price": "$1.00",
+            "img_url": "http://s3-media2.fl.yelpcdn.com/bphoto/B0nM9-EAzBIy3BAcFg0Y9g/o.jpg",
             "available": true
           },
           {
-            "item": "Cooks",
+            "item": "Cookies",
             "price": "$0.50",
+            "img_url": "http://www.mcdonalds.com/content/dam/McDonalds/item/mcdonalds-Chocolate-Chip-Cookie.png",
             "available": true
           }
         ]
